1f9fa00ca16fcee7544c20f2feb1c4795b6a9c0f
sridharpoddar <sridharpoddar@gmail.com>
2017-05-18 06:30:19 -0700

    added javascript to move the images when keys are pressed.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project-00
 first project game
+### Have a hare and a tortoise and have two people race by them side-by-side 
+### Have one person click on enter key and the other click on capslock key to let the items start moving 
+### Assign a random speed to the objects and have the objects stop when they let go of the key or if they hit the key again, depending on the format 
--- main.js
+++ main.js
@@ -1,2 +1,58 @@
 // sanity check 
 console.log("hi!")
+$(document).ready(function(e) {
+    
+
+
+
+$(window).on("keydown", function moveHare(event){
+
+	if (event.keyCode==13){
+	$("#hare").appendTo("#cent-left")}
+	
+	// $("#hare").css({"float": "right",
+	// "margin": "180px 0 0 0",
+	// "height": "200px",
+	// "width": "220px"
+	// "transform": scale(-1, 1);})
+
+
+	}); 
+
+$(window).on("keydown", function moveTort(event){
+
+	if (event.keyCode==20){
+	$("#tort").appendTo("#cent-right")}
+	
+	// $("#hare").css({"float": "right",
+	// "margin": "180px 0 0 0",
+	// "height": "200px",
+	// "width": "220px"
+	// "transform": scale(-1, 1);})
+
+
+	})
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+// .css({
+//     position: 'absolute',
+//     left: 169,
+//     top: 182
+
+// $(this).rules('add', {
+// 		float: left,
+// 		margin: 0 0 0 0
+
+// 	});
